791b26411bd527a4f9511c046c078ac65b732015
Oscar Neira <oneirag@yahoo.es>
2025-05-27 23:22:37 +0200

    Added cloud support for tinytuya device. Also corrected dps assignment in tinytuya as it was completely incorrect.

--- ong_meter_data/update_meter.py
+++ ong_meter_data/update_meter.py
@@ -8,7 +8,9 @@ import threading
 from ong_tsdb.client import OngTsdbClient
 from ong_utils import OngTimer
 from tinytuya.Contrib.WiFiDualMeterDevice import WiFiDualMeterDevice
+from tuya_connector import TuyaOpenAPI
 
+from ong_meter_data.update_i_de import SECONDS_SLEEP
 
 timer = OngTimer(False)
 
@@ -195,14 +197,17 @@ class TinyTuyaDevice(MeteringDevice):
             address=base_url,
             version=3.4 
         )
-        self.last_status = None 
-        data = self.device.status()
-        if data:
-            logger.debug(f"Connected to device {self.name} and received {data=}")
-        else:
-            logger.error("Could not connect to device " + self.name)
+        self.last_status = None
+        if pwd:     # If no password is supplied then no need to test connection as it will be child class
+            data = self.device.status()
+            if data:
+                logger.debug(f"Connected to device {self.name} and received {data=}")
+            else:
+                logger.error("Could not connect to device " + self.name)
 
-    def read_meter_url(self):
+
+    def get_status(self) -> dict:
+        """Returns current device status. it is a dict with two keys, dps and timestamp, like {"dps: {'1': 0, '101': 542.3...}"""
         #  DP_REFRESH: 18, // Request refresh of DPS  UPDATEDPS / LAN_QUERY_DP
         # payload = wdm.generate_payload(command=18, data=[4, 5, 6, 18, 19, 20])
         # print(wdm.receive())
@@ -213,6 +218,11 @@ class TinyTuyaDevice(MeteringDevice):
         # from time import sleep
         # sleep(5)
         status = self.device.status()
+        status['timestamp'] = time.time_ns()
+        return status
+
+    def read_meter_url(self):
+        status = self.get_status()
         if status != self.last_status:
             logger.debug(f"Status changed for {self.name} from {self.last_status} to {status}")
             self.last_status = status
@@ -233,12 +243,76 @@ class TinyTuyaDevice(MeteringDevice):
                 retval.append(numeric_value)
             return retval
         
-        self.timestamp_ns = time.time_ns()
+        self.timestamp_ns = status['timestamp']
         self.data['active_power'] = get_values_circuits("DPS_POWER_", status_data=status)
         self.data['current'] = get_values_circuits("DPS_CURRENT_", status_data=status)
         self.data['voltage'] = get_values_circuits("DPS_VOLTAGE", status_data=status)
         logger.debug("Leido " + self.name)
         
+class TuyaCloudDevice(TinyTuyaDevice):
+    def __init__(self, name, cloud_api_base_url, circuits: tuple, access_id, access_key, device_id):
+        """
+        Inits the class that reads metering device
+        :param name: name of the metering device (to use as measurement name in influx)
+        :param cloud_api_base_url: base url for the tuya cloud
+        :param circuits: list of circuits names in the same order as they appear in the meter
+        :param access_id: application access_id for the cloud
+        :param access_key: application access_key for the cloud
+        :param device_id: device_id to be read from the cloud
+        """
+        super().__init__(name, "", circuits, usr=device_id)
+        # Init OpenAPI and connect
+        self.openapi = TuyaOpenAPI(cloud_api_base_url, access_id, access_key)
+        res = self.openapi.connect()
+        if res['success']:
+            logger.info("Connected to cloud")
+        else:
+            raise ConnectionError("Could not connect to cloud api. Review cloud_api_base_url, access_id and access_key")
+        self.DEVICE_ID = device_id
+
+        # Call any API from Tuya
+        response = self.openapi.get("/v1.0/statistics-datas-survey", dict())
+        print(response)
+
+        print(response := self.openapi.get(f"/v1.0/devices/datas-survey", dict()))
+        # print(response := self.openapi.get(f"/v1.0/devices", dict(page_no=0, page_size=10)))
+        print(response := self.openapi.get(f"/v1.0/devices/{self.DEVICE_ID}", dict()))
+
+        # Gets MAC address
+        self.openapi.get("/v1.0/devices/factory-infos", dict(device_ids=self.DEVICE_ID))
+
+        # logs_types=",".join(map(str, range(1, 11)))
+        # start_t = int(datetime.datetime.today().replace(hour=1).timestamp()*1000)
+        # end_t = int(datetime.datetime.today().timestamp()*1000)
+        # print(response := openapi.get(f"/v1.0/devices/{DEVICE_ID}/logs", dict(type=logs_types,
+        #                               start_time=start_t, end_time=end_t, size=100)))
+
+
+    def get_status(self) -> dict:
+        last_data = self.openapi.get(f"/v2.0/cloud/thing/{self.DEVICE_ID}/shadow/properties", dict())
+        last_status = last_data['result']['properties']
+        timestamps = {str(d['dp_id']): d['time'] for d in last_status}
+        last_timestamp = max(timestamps.values())
+        # Power A is 111
+        # Power B is 106
+        # Frequency is 102
+        # Voltage is 101
+        dps = {str(d['dp_id']): d['value'] for d in last_status}
+        # My device has different settings as the standard
+        self.device.DPS_POWER_A = "111"
+        self.device.DPS_POWER_B = "106"
+        self.device.DPS_FREQ = "102"
+        self.device.DPS_VOLTAGE = "101"
+        self.device.DPS_CURRENT_A = "110"
+        self.device.DPS_CURRENT_B = "105"
+
+        self.device.dps_data[self.device.DPS_POWER_A]['scale'] = 10     # Instead of 100
+        self.device.dps_data[self.device.DPS_POWER_B]['scale'] = 10     # Instead of 100
+        self.device.dps_data[self.device.DPS_CURRENT_A]['scale'] = 1000    # In Amps (original reading is in mA)
+        self.device.dps_data[self.device.DPS_CURRENT_B]['scale'] = 1000  # In Amps (original reading is in mA)
+
+        timestamp = int(last_timestamp * 1e6)
+        return dict(dps=dps, timestamp=timestamp)
 
 
 meters = dict()
@@ -250,9 +324,14 @@ meters = dict()
 #                                   circuits=('general', 'lavadora', 'nevera'),
 #                                   usr=config("meter_user"), pwd=config("meter_pwd")
 #                                   )
-meters['tinytuya'] = TinyTuyaDevice('tinytuya', base_url=config("tinytuya_wdm_base_url"),
+# meters['tinytuya'] = TinyTuyaDevice('tinytuya', base_url=config("tinytuya_wdm_base_url"),
+#                                     circuits=('general', 'lavadora'),
+#                                     usr=config("tinytuya_wdm_usr"), pwd=config("tinytuya_wdm_pwd")
+#                                     )
+meters['tinytuya'] = TuyaCloudDevice('tinytuya', cloud_api_base_url=config("tuyacloud_api_endoint"),
                                     circuits=('general', 'lavadora'),
-                                    usr=config("tinytuya_wdm_usr"), pwd=config("tinytuya_wdm_pwd")
+                                    access_id=config("tuyacloud_access_id"), access_key=config("tuyacloud_access_key"),
+                                    device_id=config("tuyacloud_device_id")
                                     )
 
 
@@ -261,6 +340,7 @@ _bucket = config('bucket')
 ongtsdb_client = OngTsdbClient(url=config('url'), token=config('admin_token'), validate_server_version=False)
 ongtsdb_client.create_db(_bucket)
 for sensor in meters.keys():
+    # ongtsdb_client.delete_sensor(_bucket, sensor)
     ongtsdb_client.create_sensor(_bucket, sensor, "1s", metrics=list(),
                                  read_key=config('read_token'), write_key=config('write_token'))
 
@@ -279,4 +359,5 @@ while True:
         except Exception as e:
             logger.error(e)
     logger.debug("Elapsed Time: %s" % (time.time() - start_t))
-    time.sleep(max(0.0, start_t + 1 - time.time()))
+    SECONDS_SLEEP = 10      # To avoid problems with cloud (previously I used every second)
+    time.sleep(max(0.0, start_t + SECONDS_SLEEP - time.time()))
