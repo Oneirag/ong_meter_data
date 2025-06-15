8b5ddb6907877a941071db9588f5e5ed6e9c70ce
Oscar Neira <oscar.neira@bau360.es>
2025-06-15 07:18:08 +0000

    version updated to 0.0.3

--- src/ong_meter_data/__init__.py
+++ src/ong_meter_data/__init__.py
@@ -1,6 +1,6 @@
 from ong_utils import OngConfig, LOCAL_TZ, create_pool_manager
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 _util = OngConfig("ong_meter_data", cfg_filename="ong_config.yml")
 logger = _util.logger
