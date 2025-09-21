5bf01aadbd89599de27019712bbc85ba157e3a6d
Oscar Neira Garcia <oneirag@yahoo.es>
2025-09-21 23:48:40 +0200

    Promote to version 0.0.6, move some variables to __init__ and make lazy init ofr ongtsdb_client

--- src/ong_meter_data/__init__.py
+++ src/ong_meter_data/__init__.py
@@ -1,8 +1,12 @@
 from ong_utils import OngConfig, LOCAL_TZ, create_pool_manager
 from pathlib import Path
+from ong_tsdb.client import OngTsdbClient
+from ong_utils import OngTimer
+from functools import lru_cache
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
+timer = OngTimer(False)
 _util = OngConfig("ong_meter_data", cfg_filename="ong_config.yml")
 logger = _util.logger
 config = _util.config
@@ -14,3 +18,11 @@ __COOKIES_FILE = "i-de_cookies.json"
 config_file_path = Path("~/.config/ongpi/").expanduser()
 config_file_path.mkdir(parents=True, exist_ok=True)
 JSON_CONFIG_FILE = config_file_path / __COOKIES_FILE
+
+_bucket = config('bucket')
+
+
+@lru_cache(maxsize=None)
+def init_ongtsdb_client():
+    ongtsdb_client = OngTsdbClient(url=config('url'), token=config('admin_token'), validate_server_version=False)
+    return ongtsdb_client
